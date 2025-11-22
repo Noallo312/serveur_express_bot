981eaaef5713b379f9739a878975a300cb0c4a25
Noallo312 <limourinoa@gmail.com>
2025-11-22 12:42:07 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,4 @@
-from main import app
+from bot import app
 
 if __name__ == "__main__":
     app.run()
-```
-
-3. **Vérifie que ton `Procfile` dit** :
-```
-web: gunicorn --bind 0.0.0.0:$PORT --workers 1 --timeout 120 wsgi:app
