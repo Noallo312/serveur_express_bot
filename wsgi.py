d6a00fc263e757e0bc9d9329a6477ea811c6192a
Noallo312 <limourinoa@gmail.com>
2025-11-22 12:29:26 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,10 @@
-from bot import app
+from app import app
 
 if __name__ == "__main__":
     app.run()
+```
+
+2. **Mets à jour ton `Procfile`** :
+```
+release: python -c "print('Ready')"
+web: python app.py
